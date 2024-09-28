1280273f8777e5c0299da1d46d09bcfa8b617197
Hidalgo87 <juanfgomez6@gmail.com>
2024-09-28 15:44:42 -0500

    FINALLLYYYYY supabase is working, im really happy, the image service is finally working

--- src/app/features/images/services/image.service.ts
+++ src/app/features/images/services/image.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
 
-  constructor() { }
+  private supabase: SupabaseClient;
+
+  constructor() { 
+    this.supabase = createClient('https://poahqglqttfchvblwoav.supabase.co',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvYWhxZ2xxdHRmY2h2Ymx3b2F2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNjcwMTgxOCwiZXhwIjoyMDQyMjc3ODE4fQ.HTae8oq6by4C5llIBjyzGQSVX5j52vux6XbfO09rsk8'
+    )
+   }
+
+   async upload(file:File, fileName:string, folderName:string='base'){
+    const { error } = await this.supabase
+    .storage
+    .from('airScapeBKT')
+    .upload(`${folderName}/${fileName}`, file);
+    if (error){
+      alert(error.message);
+      return;
+    }
+    const { data } = await this.supabase
+    .storage
+    .from('airScapeBKT')
+    .getPublicUrl(`${folderName}/${fileName}`)
+    return data.publicUrl;
+   }
 }
--- src/app/features/listings/services/listings.service.ts
+++ src/app/features/listings/services/listings.service.ts
@@ -1,34 +1,20 @@
 import { Injectable } from '@angular/core';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { environment } from '../../../../environments/environment';
+import { ImageService } from '../../images/services/image.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ListingsService {
-  supabase: SupabaseClient;
 
-  constructor() {
-    this.supabase = createClient(
-      environment.supabaseConfig.url,
-      environment.supabaseConfig.apikey
-    );
+  constructor(private imageService:ImageService) {
+
   }
 
   // Upload file using standard upload
   async uploadFile(file: File, userName: string, fileName: string) {
-    const { data, error } = await this.supabase.storage
-      .from('listing_photos')
-      .upload(`${userName}/${fileName}`, file);
-    if (error) {
-      // Handle error
-    } else {
-      // Handle success
-    }
-
-    // Obtener url de la imagen
-    const resp = await this.supabase.storage
-      .from('listing_photos')
-      .getPublicUrl(`${userName}/${fileName}`);
+    const data = await this.imageService.upload(file,userName, fileName);
+    console.log('data', data);
   }
 }
