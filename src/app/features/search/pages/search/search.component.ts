46e5922078ef0528fbf2e986ebdd7f1f1ece0870
Wilson <wilson.santiago.car@gmail.com>
2024-10-01 11:58:26 -0500

    query params integrated on search component

--- src/app/features/search/pages/search/search.component.ts
+++ src/app/features/search/pages/search/search.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
 import { SliderModule } from 'primeng/slider';
 import { FormsModule } from '@angular/forms';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { DropdownModule } from 'primeng/dropdown';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { octFilter, octSortDesc } from '@ng-icons/octicons';
+import { octCheck, octFilter, octSortDesc } from '@ng-icons/octicons';
 import { ListingCardComponent } from '../../../listings/components/listing-card/listing-card.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ListingsService } from '../../../listings/services/listings.service';
+
+interface Option {
+  name: string;
+}
 
 @Component({
   selector: 'app-search',
@@ -20,13 +26,50 @@ import { ListingCardComponent } from '../../../listings/components/listing-card/
     NgIconComponent,
     ListingCardComponent,
   ],
-  providers: [provideIcons({ octFilter, octSortDesc })],
+  providers: [provideIcons({ octFilter, octSortDesc, octCheck })],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css',
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
   price: number | undefined;
-  sort: string | undefined;
+  sort: Option | undefined;
 
-  sortOptions = [{ name: 'Price' }, { name: 'Most recent' }];
+  filterPrice: number | undefined;
+  sortOption: string | undefined;
+
+  sortOptions: Option[] = [{ name: 'Price' }, { name: 'Most recent' }];
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router // private listingService: ListingsService
+  ) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      this.filterPrice = params['price'] || null;
+      this.sortOption = params['sort'] || null;
+
+      this.applyFiltersAndSort();
+    });
+  }
+
+  applyFiltersAndSort(): void {
+    console.log(`Filter: ${this.filterPrice}, Sort: ${this.sortOption}`);
+  }
+
+  onFilterChange(): void {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { price: this.price },
+      queryParamsHandling: 'merge',
+    });
+  }
+
+  onSortChange(): void {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { sort: this.sort?.name || '' },
+      queryParamsHandling: 'merge',
+    });
+  }
 }
