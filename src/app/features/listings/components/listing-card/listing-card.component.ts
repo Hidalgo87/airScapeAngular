4d5ad5f36e4d5b7c7c0b449da0e7997cda0a0e2a
Wilson <wilson.santiago.car@gmail.com>
2024-10-01 21:57:13 -0500

    added guests display on listing card

--- src/app/features/listings/components/listing-card/listing-card.component.ts
+++ src/app/features/listings/components/listing-card/listing-card.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ListingBrief } from '../../interfaces/listingBrief.interface';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { octPerson } from '@ng-icons/octicons';
 
 @Component({
   selector: 'app-listing-card',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIconComponent],
+  providers: [provideIcons({ octPerson })],
   templateUrl: './listing-card.component.html',
   styleUrl: './listing-card.component.css',
 })
