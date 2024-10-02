d2c3cd3cf20d2ce4777f4a2db1a474d466a511db
Hidalgo87 <juanfgomez6@gmail.com>
2024-10-01 22:47:38 -0500

    removed

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  supabaseConfig: {
-    url: 'https://poahqglqttfchvblwoav.supabase.co',
-    apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvYWhxZ2xxdHRmY2h2Ymx3b2F2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNjcwMTgxOCwiZXhwIjoyMDQyMjc3ODE4fQ.HTae8oq6by4C5llIBjyzGQSVX5j52vux6XbfO09rsk8',
-  },
-};
