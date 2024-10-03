c303a6a28a84f2b4e14ecee3219748b61e2e97ba
Wilson <wilson.santiago.car@gmail.com>
2024-10-03 11:10:18 -0500

    home component integrated with service

--- src/app/features/home/pages/home/home.component.ts
+++ src/app/features/home/pages/home/home.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SearchBarComponent } from '../../../search/components/search-bar/search-bar.component';
 import { ListingCardComponent } from '../../../listings/components/listing-card/listing-card.component';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { octNorthStar } from '@ng-icons/octicons';
+import { ListingsService } from '../../../listings/services/listings.service';
+import { ListingBrief } from '../../../listings/interfaces/listingBrief.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SearchBarComponent, ListingCardComponent, NgIconComponent],
+  imports: [
+    SearchBarComponent,
+    ListingCardComponent,
+    NgIconComponent,
+    CommonModule,
+  ],
   providers: [provideIcons({ octNorthStar })],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  listings: ListingBrief[] = [];
+
+  constructor(private listingService: ListingsService) {}
+
+  ngOnInit(): void {
+    this.listings = this.listingService.getPopularListings();
+  }
+}
