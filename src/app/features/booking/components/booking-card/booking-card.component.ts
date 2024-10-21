6539e0cde4047819fc5d40987d3f747f50ce7fdf
Wilson <wilson.santiago.car@gmail.com>
2024-10-21 16:54:31 -0500

    booking card layout done

--- src/app/features/booking/components/booking-card/booking-card.component.ts
+++ src/app/features/booking/components/booking-card/booking-card.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { Component, Input } from '@angular/core';
+import { octComment } from '@ng-icons/octicons';
+import { BookingCard } from '../../interfaces/booking-card';
 
 @Component({
   selector: 'app-booking-card',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
+  providers: [provideIcons({ octComment })],
   templateUrl: './booking-card.component.html',
-  styleUrl: './booking-card.component.css'
+  styleUrl: './booking-card.component.css',
 })
 export class BookingCardComponent {
+  @Input() booking!: BookingCard;
 
+  ngOnInit(): void {
+    if (this.booking?.bookedAt) {
+      this.booking.bookedAt = new Date(this.booking?.bookedAt);
+    }
+    if (this.booking?.bookingStart) {
+      this.booking.bookingStart = new Date(this.booking?.bookingStart);
+    }
+    if (this.booking?.bookingEnd) {
+      this.booking.bookingEnd = new Date(this.booking?.bookingEnd);
+    }
+  }
 }
