c3fcb4dceb528bb7ed8f465b6f27b6c556f7bc0e
Wilson <wilson.santiago.car@gmail.com>
2024-10-21 19:48:43 -0500

    booking card done

--- src/app/features/booking/pages/user-bookings/user-bookings.component.ts
+++ src/app/features/booking/pages/user-bookings/user-bookings.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BookingCardComponent } from '../../components/booking-card/booking-card.component';
+import { BookingCard } from '../../interfaces/booking-card';
 
 @Component({
   selector: 'app-user-bookings',
@@ -8,4 +9,26 @@ import { BookingCardComponent } from '../../components/booking-card/booking-card
   templateUrl: './user-bookings.component.html',
   styleUrl: './user-bookings.component.css',
 })
-export class UserBookingsComponent {}
+export class UserBookingsComponent implements OnInit {
+  booking: BookingCard;
+
+  constructor() {
+    this.booking = {
+      bookingId: 1,
+      status: 'pending',
+      hostName: 'Ewits',
+      hostPicture:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Outdoors-man-portrait_%28cropped%29.jpg/1200px-Outdoors-man-portrait_%28cropped%29.jpg',
+      bookedAt: new Date(2024, 8, 5),
+      bookingStart: new Date(2024, 11, 5),
+      bookingEnd: new Date(2024, 11, 6),
+      price: 2000000,
+      propertyImage:
+        'https://definicion.de/wp-content/uploads/2011/01/casa-2.jpg',
+      propertyTitle:
+        'This is an awesome property with an outstanding landscape aaaaaaaaaaaaa',
+    };
+  }
+
+  ngOnInit() {}
+}
